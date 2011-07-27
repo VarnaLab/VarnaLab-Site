6b670b2cf786620272432b1108076cef97bf615c
Radoslav Stankov <rstankov@gmail.com>
2011-07-27 03:05:13 +0300

    Add link to parent page

--- app/helpers/admin/pages_helper.rb
+++ app/helpers/admin/pages_helper.rb
@@ -2,4 +2,8 @@ module Admin::PagesHelper
   def link_to_adding_a_subpage page
     link_to 'add subpage', new_admin_page_path(:page => {:parent_id => page.id}), :title => "Add a subpage of '#{page.name}'"
   end
+
+  def link_to_parent_page page
+    link_to 'Back', page.parent_id.present? ? admin_page_path(page.parent_id) : admin_pages_path
+  end
 end
