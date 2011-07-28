bc26de63fded88c457e6c13e7ce5512c41fa2324
Radoslav Stankov <rstankov@gmail.com>
2011-07-29 00:39:40 +0300

    After page update redirect to the appropriate location

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,9 +1,13 @@
 class Admin::PagesController < Admin::BaseController
   inherit_resources
 
-  actions :show, :new, :create, :edit, :update, :destroy
+  actions :show, :new, :create, :update, :edit, :destroy
 
   def index
     @pages = Page.root
+  end
+
+  def update
+    update! :location => resource.root? ? admin_pages_path : admin_page_path(resource.parent)
   end
 end
--- spec/controllers/admin/pages_controller_spec.rb
+++ spec/controllers/admin/pages_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Admin::PagesController do
+  let(:page) { Factory.stub(:page) }
+
   describe "GET index" do
     it "assigns the root pages" do
       Page.should_receive(:root).and_return 'root_pages'
@@ -9,5 +11,30 @@ describe Admin::PagesController do
 
       controller.should assign_to(:pages).with('root_pages')
     end
+  end
+
+  describe "PUT update" do
+    it "redirects to admin pages path if page is a root page" do
+      page.stub :root? => true
+
+      preform_update_on page
+
+      response.should redirect_to(admin_pages_url)
+    end
+
+    it "redirects to the page's parent admin page if the page is not a root page" do
+      page.stub :root? => false, :parent => Factory.stub(:page)
+
+      preform_update_on page
+
+      response.should redirect_to(admin_page_url(page.parent))
+    end
+  end
+
+  def preform_update_on(model, options = {:successful => true})
+    model.stub :update_attributes => options[:successful]
+    controller.stub :resource => model
+
+    put :update, :id => model.id
   end
 end
