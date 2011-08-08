9840b25419fa39ba1426402d7a989f41cb193699
Radoslav Stankov <rstankov@gmail.com>
2011-08-09 02:36:52 +0300

    Add very basic email validation for the comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
 
   validates_presence_of :commenter_name, :commenter_email, :commentable, :body
+  validates_format_of :commenter_email, :with => /.+@.+\.\w+/
 
   attr_protected :hidden
 
