bd84070c0fe7d7b5a6e4ccc3fecc37bad821402a
Radoslav Stankov <rstankov@gmail.com>
2011-08-14 01:13:21 +0300

    Handle record not found exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  private
+
+  def record_not_found
+    render 'shared/_record_not_found' , :layout => 'application', :status => 404
+  end
 end
