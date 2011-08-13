272fed6342369d54ba2763a40452eaa8b11b71ea
Radoslav Stankov <rstankov@gmail.com>
2011-08-13 16:51:40 +0300

    Add better input for selecting parent pages

--- lib/extensions/simple_form.rb
+++ lib/extensions/simple_form.rb
@@ -1,7 +1,14 @@
 module Extensions
   module SimpleForm
     module FormBuilder
+      def parent_input(options = {})
+        name = options.delete(:name_attribute) { :name }
 
+        options[:collection] ||= template.nested_set_options(object.class, object) { |i| "#{'-' * i.level} #{i.send(name)}" }
+        options[:prompt] ||= '-- no parent --'
+
+        input :parent_id, options
+      end
     end
   end
 end
