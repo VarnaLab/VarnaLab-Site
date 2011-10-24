ecc4c02359bbcc163abc79ad48770bb4e5de4890
Radoslav Stankov <rstankov@gmail.com>
2011-10-24 23:29:59 +0300

    Improve the comments presenter

--- app/presenters/admin/comments_presenter.rb
+++ app/presenters/admin/comments_presenter.rb
@@ -1,27 +1,35 @@
 class Admin::CommentsPresenter
-  attr_reader :comments
-
   def initialize(options = {})
-    @review = options[:review] || 'all'
-    @commentable = options[:commentable]
-    @scope = @commentable.presence.try(:comments) || Comment
-    @comments = comments_scope.order('created_at DESC').page(options[:page])
+    @options = options
+  end
+
+  def comments
+    @comments ||= comments_scope.order('created_at DESC').page(@options[:page] || 1)
   end
 
   def title
-    title = "#{@review.titleize} comments"
-    title << " for #{@commentable.name}" if @commentable.present?
+    title = "#{review.titleize} comments"
+    title << " for #{commentable.name}" if commentable.present?
     title
   end
 
   private
 
+  def review
+    @options[:review] || 'all'
+  end
+
+  def commentable
+    @options[:commentable]
+  end
+
   def comments_scope
-    case @review
-      when 'visible'    then @scope.visible
-      when 'hidden'     then @scope.hidden
-      when 'unreviewed' then @scope.unreviewed
-      when 'all'        then @scope
+    scope = commentable.presence.try(:comments) || Comment
+    case review
+      when 'visible'    then scope.visible
+      when 'hidden'     then scope.hidden
+      when 'unreviewed' then scope.unreviewed
+      when 'all'        then scope
       else raise "Invalid review state - #{options[:review]}"
     end
   end
