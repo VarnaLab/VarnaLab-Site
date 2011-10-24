17d7a94d110e8238cff602e59fc88f2440641a76
Radoslav Stankov <rstankov@gmail.com>
2011-10-24 23:30:29 +0300

    Add ability to reorder pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,9 +7,20 @@ class Page < ActiveRecord::Base
   acts_as_nested_set
 
   scope :visible, where(:visible => true)
+  scope :ordered, order('lft ASC')
+
+  class << self
+    def reorder(ids)
+      return if ids.blank?
+      attributes = where(:id => ids).ordered.map { |page| {:lft => page.lft, :rgt => page.rgt} }
+      ids.each_with_index do |id, index|
+        Page.update_all(attributes[index], :id => id)
+      end
+    end
+  end
 
   def visible_children
-    children.visible
+    children.visible.ordered
   end
 
   def hidden?
