de065af8669e2dc3e10866c64338f82f121ae290
MKostova <mkostova@camplight.net>
2012-05-19 18:00:37 +0300

    spec deprecations fix

---
+++ spec/presenters/admin/comments_presenter_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+module Admin
+  describe CommentsPresenter do
+    describe "title" do
+      it "is the review when no commentable is given" do
+        CommentsPresenter.new.title.should == 'All comments'
+        CommentsPresenter.new(:review => 'visible').title.should == 'Visible comments'
+      end
+
+      it "is the commentable name + review when commentable is given" do
+        commentable = double(:name => 'Commentable')
+
+        CommentsPresenter.new(:commentable => commentable).title.should == 'All comments for Commentable'
+        CommentsPresenter.new(:commentable => commentable, :review => 'unreviewed').title.should == 'Unreviewed comments for Commentable'
+        CommentsPresenter.new(:commentable => commentable, :review => 'visible').title.should == 'Visible comments for Commentable'
+      end
+    end
+
+    describe "finding comments" do
+      it "shows all comments by default" do
+        reviewed_comment = FactoryGirl.create(:comment, :reviewed => true)
+        unreviewed_comment = FactoryGirl.create(:comment, :reviewed => false)
+
+        comments = CommentsPresenter.new.comments
+        comments.should include(reviewed_comment)
+        comments.should include(unreviewed_comment)
+      end
+
+      it "can select unreviewed comments" do
+        reviewed_comment = FactoryGirl.create(:comment, :reviewed => true)
+        unreviewed_comment = FactoryGirl.create(:comment, :reviewed => false)
+
+        CommentsPresenter.new(:review => 'unreviewed').comments.should == [unreviewed_comment]
+      end
+
+      it "can select visible or hidden comments" do
+        visible_comment = FactoryGirl.create(:comment, :hidden => false)
+        hidden_comment = FactoryGirl.create(:comment, :hidden => true)
+
+        CommentsPresenter.new(:review => 'visible').comments.should == [visible_comment]
+        CommentsPresenter.new(:review => 'hidden').comments.should == [hidden_comment]
+      end
+
+      it "can show only comments for given commentable" do
+        comment_1 = FactoryGirl.create(:comment)
+        comment_2 = FactoryGirl.create(:comment)
+
+        CommentsPresenter.new(:commentable => comment_1.commentable).comments.should == [comment_1]
+        CommentsPresenter.new(:commentable => comment_2.commentable).comments.should == [comment_2]
+      end
+    end
+  end
+end
