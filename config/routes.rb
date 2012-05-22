783b05fa324d65d6c77dd93731b253e35e145393
MKostova <mkostova@camplight.net>
2012-05-22 22:31:54 +0300

    fix in routes - admin/comments

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,14 @@ VarnaLab::Application.routes.draw do
     match '/page/:page_id/comments/(:review)', :to => 'comments#index', :as => 'page_comments'
     
     resources :users, :controller => 'users' do
-      match '/users/index', :to => 'users#index', :as => '', :on => :collection, :constraints => { :index => /[a-z]+ / }
+      match '/users/index', :to => 'users#index', :as => '', :on => :collection, :constraints => { :index => /[a-z]+/ }
     end
     
     resources :comments, :only => [:destroy] do
       post :review, :to => 'comments/reviews#create'
       delete :review, :to => 'comments/reviews#destroy'
 
-     match '/(:review)', :to => 'comments#index', :as => '', :on => :collection, :constraints => { :review => /[a-z]+ / }
+     match '/(:review)', :to => 'comments#index', :as => '', :on => :collection, :constraints => { :review => /[a-z]+/ }
     end
   end
 
